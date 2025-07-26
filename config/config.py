ac8b775439d2d00107e292af8e55132221504330
张浩田 <23121213864@stu.xidian.edu.cn>
2025-07-26 16:05:00 +0800

    Add files via upload

--- config/config.py
+++ config/config.py
@@ -3,16 +3,19 @@ import torch
 
 class Config:
     # 数据路径
-    TRA_DATA_ROOT1 = '../training_dataset_esm'
-    TRA_DATA_ROOT2 = '../based_structure_training_dataset'
-    VAL_DATA_ROOT1 = '../val_dataset_esm'
-    VAL_DATA_ROOT2 = '../based_structure_val_dataset'
-
+    TRA_DATA_ROOT1 = './training_dataset_esm'
+    TRA_DATA_ROOT2 = './based_structure_training_dataset'
+    TRA_DATA_ROOT3 = './residue_interface/esm2/training_dataset_esm'
+    TRA_DATA_ROOT4 = './residue_interface/esmif/training_dataset_esmif'
+    VAL_DATA_ROOT1 = './val_dataset_esm'
+    VAL_DATA_ROOT2 = './based_structure_val_dataset'
+    VAL_DATA_ROOT3 = './residue_interface/esm2/val_dataset_esm'
+    VAL_DATA_ROOT4 = './residue_interface/esmif/val_dataset_esmif'
     # 模型参数
     INPUT_SIZE = 1280
     HIDDEN_SIZE_1 = 256
     HIDDEN_SIZE_2 = 128
-    HIDDEN_CHANNELS_INPUT = 256000
+    HIDDEN_CHANNELS_INPUT = 307200
     IN_CHANNELS = 40  # 节点特征维度
     HIDDEN_CHANNELS_1 = 256
     HIDDEN_CHANNELS_2 = 128
@@ -23,9 +26,9 @@ class Config:
     NUM_GAT_HEADS = 2
     # 训练参数
     EPOCHS = 100
-    BATCH_SIZE = 16
+    BATCH_SIZE = 32
     LEARNING_RATE = 0.0001
-    WEIGHT_DECAY = 0.001
+    WEIGHT_DECAY = 0.005
     DROPOUT_RATE = 0.5
 
     # 其他参数
